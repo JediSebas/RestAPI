2ef8e8d89b49c2d22076431af042ef49b656b30b
sebastian.dolata <sebasdolata04@gmail.com>
2023-03-15 19:27:43 +0100

    Added create PersonalDetails in controller

--- src/main/java/com/jedisebas/restapi/controller/PersonalDetailsController.java
+++ src/main/java/com/jedisebas/restapi/controller/PersonalDetailsController.java
@@ -1,12 +1,20 @@
 package com.jedisebas.restapi.controller;
 
+import com.jedisebas.restapi.entity.PersonalDetails;
 import com.jedisebas.restapi.service.PersonalDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/persons")
 public class PersonalDetailsController {
 
     @Autowired
     private PersonalDetailsService service;
+
+    @PostMapping()
+    public String create(@RequestBody final PersonalDetails personalDetails) {
+        return service.createPersonalDetails(personalDetails).getId().toString();
+    }
 }
