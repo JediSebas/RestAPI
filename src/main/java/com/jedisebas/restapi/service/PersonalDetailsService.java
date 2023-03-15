99156bc5cc1e968fe6cf7e18820c6c3a36c9032a
sebastian.dolata <sebasdolata04@gmail.com>
2023-03-15 19:21:57 +0100

    Added create PersonalDetails in service

--- src/main/java/com/jedisebas/restapi/service/PersonalDetailsService.java
+++ src/main/java/com/jedisebas/restapi/service/PersonalDetailsService.java
@@ -5,23 +5,13 @@ import com.jedisebas.restapi.repository.PersonalDetailsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
 public class PersonalDetailsService {
 
     @Autowired
     private PersonalDetailsRepository repository;
 
-    public List<PersonalDetails> fetchAllPersonalDetails() {
-        return repository.findAll();
-    }
-
-    public void savePersonalDetails(PersonalDetails personalDetails) {
-        repository.save(personalDetails);
-    }
-
-    public void saveAllPersonalDetails(List<PersonalDetails> personalDetailsList) {
-        repository.saveAll(personalDetailsList);
+    public PersonalDetails createPersonalDetails(final PersonalDetails personalDetails) {
+        return repository.save(personalDetails);
     }
 }
