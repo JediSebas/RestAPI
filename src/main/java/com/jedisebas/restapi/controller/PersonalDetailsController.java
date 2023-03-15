a5834e76cceabd8ad745214d4d0a12336355390e
sebastian.dolata <sebasdolata04@gmail.com>
2023-03-15 20:17:33 +0100

    Added create in controller

--- src/main/java/com/jedisebas/restapi/controller/PersonalDetailsController.java
+++ src/main/java/com/jedisebas/restapi/controller/PersonalDetailsController.java
@@ -2,19 +2,26 @@ package com.jedisebas.restapi.controller;
 
 import com.jedisebas.restapi.entity.PersonalDetails;
 import com.jedisebas.restapi.service.PersonalDetailsService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/persons")
+
+@RestController()
 public class PersonalDetailsController {
+
+    private static final Logger LOGGER = LogManager.getLogger();
 
     @Autowired
     private PersonalDetailsService service;
 
-    @PostMapping()
-    public String create(@RequestBody final PersonalDetails personalDetails) {
-        return service.createPersonalDetails(personalDetails).getId().toString();
+    @PostMapping("/persons")
+    public PersonalDetails create(@RequestBody final PersonalDetails personalDetails) {
+        PersonalDetails p = service.createPersonalDetails(personalDetails);
+        LOGGER.info(p);
+        return p;
     }
 }
