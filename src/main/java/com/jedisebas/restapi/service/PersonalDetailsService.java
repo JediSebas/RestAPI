6d27e9755658b8c77ad0c81e0e371c97e54214e6
sebastian.dolata <sebasdolata04@gmail.com>
2023-03-15 14:16:14 +0100

    Added save and fetch method in PersonalDetailsService

--- src/main/java/com/jedisebas/restapi/service/PersonalDetailsService.java
+++ src/main/java/com/jedisebas/restapi/service/PersonalDetailsService.java
@@ -1,12 +1,27 @@
 package com.jedisebas.restapi.service;
 
+import com.jedisebas.restapi.entity.PersonalDetails;
 import com.jedisebas.restapi.repository.PersonalDetailsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class PersonalDetailsService {
-    
+
     @Autowired
     private PersonalDetailsRepository repository;
+
+    public List<PersonalDetails> fetchAllPersonalDetails() {
+        return repository.findAll();
+    }
+
+    public void savePersonalDetails(PersonalDetails personalDetails) {
+        repository.save(personalDetails);
+    }
+
+    public void saveAllPersonalDetails(List<PersonalDetails> personalDetailsList) {
+        repository.saveAll(personalDetailsList);
+    }
 }
