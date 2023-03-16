12b084bfce18dd490b974eb80feb81939ae3506c
sebastian.dolata <sebasdolata04@gmail.com>
2023-03-16 16:48:31 +0100

    Added anonymize to optimise later

--- src/main/java/com/jedisebas/restapi/dto/PersonalDetailsDto.java
+++ src/main/java/com/jedisebas/restapi/dto/PersonalDetailsDto.java
@@ -19,7 +19,49 @@ public class PersonalDetailsDto {
     private String email;
 
     public PersonalDetailsDto anonymize() {
-        lastName = String.valueOf(lastName.charAt(0));
+
+        lastName = anonymizeLastName(lastName);
+        email = anonymizeEmail(email);
+
         return this;
+    }
+
+    private String anonymizeLastName(final String lastName) {
+        return lastName.charAt(0) + "*".repeat(lastName.length() - 1);
+    }
+
+    private String anonymizeEmail(final String email) {
+        final StringBuilder emailBuilder = new StringBuilder();
+        emailBuilder.append(email.charAt(0));
+        emailBuilder.append("*".repeat(3));
+
+        int i = 4;
+        while (email.charAt(i + 1) != '@') {
+            i++;
+        }
+
+        emailBuilder.append(email.charAt(i));
+        i++;
+        emailBuilder.append(email.charAt(i));
+        i++;
+        emailBuilder.append(email.charAt(i));
+        i++;
+
+        emailBuilder.append("*".repeat(3));
+
+        while (email.charAt(i + 1) != '.') {
+            i++;
+        }
+
+        emailBuilder.append(email.charAt(i));
+        i++;
+        emailBuilder.append(email.charAt(i));
+        i++;
+
+        for (; i < email.length(); i++) {
+            emailBuilder.append(email.charAt(i));
+        }
+
+        return emailBuilder.toString();
     }
 }
