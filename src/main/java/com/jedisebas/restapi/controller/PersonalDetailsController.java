58edf66ec968f0aafa2997b0d6c1e32408c1eac2
sebastian.dolata <sebasdolata04@gmail.com>
2023-03-17 08:49:26 +0100

    Added 201 created status

--- src/main/java/com/jedisebas/restapi/controller/PersonalDetailsController.java
+++ src/main/java/com/jedisebas/restapi/controller/PersonalDetailsController.java
@@ -6,6 +6,7 @@ import com.jedisebas.restapi.service.PersonalDetailsService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,6 +20,7 @@ public class PersonalDetailsController {
     private PersonalDetailsService service;
 
     @PostMapping("/persons")
+    @ResponseStatus(HttpStatus.CREATED)
     public CreatedPersonResponse create(@RequestBody final PersonalDetailsDto personalDTO) {
         return service.createPersonalDetails(personalDTO);
     }
