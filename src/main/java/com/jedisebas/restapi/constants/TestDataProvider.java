15339e447e9f3c012a040058e9617dd7a1f63132
sebastian.dolata <sebasdolata04@gmail.com>
2023-03-20 10:25:30 +0100

    Added fields constants

--- src/main/java/com/jedisebas/restapi/constants/TestDataProvider.java
+++ src/main/java/com/jedisebas/restapi/constants/TestDataProvider.java
@@ -8,31 +8,36 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class TestDataProvider {
 
+    private static final String PROPERLY_FIRST_NAME = "Szymon";
+    private static final String PROPERLY_LAST_NAME = "Marciniak";
+    private static final String PROPERLY_ADDRESS = "Kalisz";
+    private static final String PROPERLY_EMAIL = "szymonmarciniak@gmail.com";
+
     public static PersonalDetails createProperlyPersonalDetails() {
         return PersonalDetails.builder()
-                .firstName("Szymon")
-                .lastName("Marciniak")
-                .address("Kalisz")
-                .email("szymonmarciniak@gmail.com")
+                .firstName(PROPERLY_FIRST_NAME)
+                .lastName(PROPERLY_LAST_NAME)
+                .address(PROPERLY_ADDRESS)
+                .email(PROPERLY_EMAIL)
                 .build();
     }
 
     public static PersonalDetails createProperlyPersonalDetailsWithId() {
         return PersonalDetails.builder()
                 .id(1)
-                .firstName("Szymon")
-                .lastName("Marciniak")
-                .address("Kalisz")
-                .email("szymonmarciniak@gmail.com")
+                .firstName(PROPERLY_FIRST_NAME)
+                .lastName(PROPERLY_LAST_NAME)
+                .address(PROPERLY_ADDRESS)
+                .email(PROPERLY_EMAIL)
                 .build();
     }
 
     public static PersonalDetailsDto createProperlyPersonalDetailsDto() {
         return PersonalDetailsDto.builder()
-                .firstName("Szymon")
-                .lastName("Marciniak")
-                .address("Kalisz")
-                .email("szymonmarciniak@gmail.com")
+                .firstName(PROPERLY_FIRST_NAME)
+                .lastName(PROPERLY_LAST_NAME)
+                .address(PROPERLY_ADDRESS)
+                .email(PROPERLY_EMAIL)
                 .build();
     }
 }
