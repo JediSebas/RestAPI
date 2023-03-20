db342cdd63a2b9733a22211d59c71f848f27ffe0
sebastian.dolata <sebasdolata04@gmail.com>
2023-03-20 09:22:27 +0100

    Fixed controller

--- src/main/java/com/jedisebas/restapi/controller/PersonalDetailsController.java
+++ src/main/java/com/jedisebas/restapi/controller/PersonalDetailsController.java
@@ -5,12 +5,15 @@ import com.jedisebas.restapi.dto.PersonalDetailsDto;
 import com.jedisebas.restapi.service.PersonalDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/v1/persons")
+@RequestMapping(path = "/v1/persons",
+        consumes = MediaType.APPLICATION_JSON_VALUE,
+        produces = MediaType.APPLICATION_JSON_VALUE)
 public class PersonalDetailsController {
 
     @Autowired
