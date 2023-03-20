ecc5a4846ef1b352f05404655778fea1723b6b99
sebastian.dolata <sebasdolata04@gmail.com>
2023-03-20 09:12:43 +0100

    Fixed controller version

--- src/main/java/com/jedisebas/restapi/controller/PersonalDetailsController.java
+++ src/main/java/com/jedisebas/restapi/controller/PersonalDetailsController.java
@@ -5,33 +5,29 @@ import com.jedisebas.restapi.dto.PersonalDetailsDto;
 import com.jedisebas.restapi.service.PersonalDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController()
+@RestController
+@RequestMapping(path = "/v1/persons")
 public class PersonalDetailsController {
 
     @Autowired
     private PersonalDetailsService service;
 
-    @PostMapping("/persons")
+    @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public CreatedPersonResponse addPersonDetails(@RequestBody final PersonalDetailsDto personalDto) {
         return service.createPersonalDetails(personalDto);
     }
 
-    @GetMapping("/persons")
+    @GetMapping
     public List<PersonalDetailsDto> getAll() {
         return service.fetchAllPersonalDetails();
     }
 
-    @GetMapping("/persons/{id}")
+    @GetMapping("/{id}")
     public PersonalDetailsDto getById(@PathVariable int id) {
         return service.fetchPersonalDetails(id);
     }
