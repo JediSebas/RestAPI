855f4e7787f6c23216e3dd17f078fa5808bdf5d9
sebastian.dolata <sebasdolata04@gmail.com>
2023-03-22 09:49:49 +0100

    Updated changelog

--- example-changelog.sql
+++ example-changelog.sql
@@ -1,29 +1,13 @@
 --liquibase formatted sql
 
---changeset your.name:1 labels:example-label context:example-context
---comment: example comment
-create table person (
-    id int primary key auto_increment not null,
-    name varchar(50) not null,
-    address1 varchar(50),
-    address2 varchar(50),
-    city varchar(30)
-)
---rollback DROP TABLE person;
+--changeset JediSebas:1
+--comment: I'm not sure what I'm doing, creating table
+CREATE TABLE personal_details(
+id INT PRIMARY KEY AUTO_INCREMENT,
+first_name VARCHAR(50),
+last_name VARCHAR(50),
+address VARCHAR(100),
+email VARCHAR(50)
+);
 
---changeset your.name:2 labels:example-label context:example-context
---comment: example comment
-create table company (
-    id int primary key auto_increment not null,
-    name varchar(50) not null,
-    address1 varchar(50),
-    address2 varchar(50),
-    city varchar(30)
-)
---rollback DROP TABLE company;
-
---changeset other.dev:3 labels:example-label context:example-context
---comment: example comment
-alter table person add column country varchar(2)
---rollback ALTER TABLE person DROP COLUMN country;
 
