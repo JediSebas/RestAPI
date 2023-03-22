e1310360cacec8e1f765be53aa7df1449c72cd2c
sebastian.dolata <sebasdolata04@gmail.com>
2023-03-22 11:02:05 +0100

    Added changes

--- src/main/resources/db/changelog/first-changelog.sql
+++ src/main/resources/db/changelog/first-changelog.sql
@@ -1,7 +1,7 @@
 --liquibase formatted sql
 
 --changeset JediSebas:1
---comment: I'm not sure what I'm doing, creating table
+--comment: Creating table personal_details
 CREATE TABLE personal_details(
 id INT PRIMARY KEY AUTO_INCREMENT,
 first_name VARCHAR(50),
@@ -9,5 +9,3 @@ last_name VARCHAR(50),
 address VARCHAR(100),
 email VARCHAR(50)
 );
-
-
