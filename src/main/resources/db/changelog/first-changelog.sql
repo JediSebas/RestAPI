4c628f7dda798944382bc42eb58244c8b53dd7fc
sebastian.dolata <sebasdolata04@gmail.com>
2023-03-22 13:27:15 +0100

    Updated changelog

--- src/main/resources/db/changelog/first-changelog.sql
+++ src/main/resources/db/changelog/first-changelog.sql
@@ -1,12 +1,12 @@
 --liquibase formatted sql
 
---changeset JediSebas:1
+--changeset JediSebas:2
 --comment: Creating table personal_details
-DROP TABLE IF EXISTS personal_details;
-CREATE TABLE personal_details(
+CREATE TABLE IF NOT EXISTS personal_details(
 id INT PRIMARY KEY AUTO_INCREMENT,
 first_name VARCHAR(50),
 last_name VARCHAR(50),
 address VARCHAR(100),
 email VARCHAR(50)
 );
+
