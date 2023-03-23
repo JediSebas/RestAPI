c5a1bb73742b5c13311b6439812c760c36a5fe24
sebastian.dolata <sebasdolata04@gmail.com>
2023-03-23 14:12:28 +0100

    Overriding a Property File

---
+++ src/test/java/com/jedisebas/restapi/PropertySourceResolverTest.java
@@ -0,0 +1,27 @@
+package com.jedisebas.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class PropertySourceResolverTest {
+
+    @Autowired
+    private PropertySourceResolver resolver;
+
+    @Test
+    void shouldTestResourceFile_overridePropertyValues() {
+        assertEquals("jdbc:h2:file:./src/main/resources/db/testdb", resolver.getSpringDatasourceUrl());
+        assertEquals("org.h2.Driver", resolver.getSpringDatasourceDriverClassName());
+        assertEquals("sa", resolver.getSpringDatasourceUsername());
+        assertEquals("password", resolver.getSpringDatasourcePassword());
+        assertEquals("org.hibernate.dialect.H2Dialect", resolver.getSpringJpaDatabasePlatform());
+        assertEquals("true", resolver.getSpringH2ConsoleEnabled());
+        assertEquals("update", resolver.getSpringJpaHibernateDdlAuto());
+        assertEquals("true", resolver.getSpringJpaShowSql());
+        assertEquals("org.hibernate.dialect.H2Dialect", resolver.getSpringJpaPropertiesHibernateDialect());
+    }
+}
