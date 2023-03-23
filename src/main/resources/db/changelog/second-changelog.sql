cae44159988194f83f9c0ed83faf9dd448542588
sebastian.dolata <sebasdolata04@gmail.com>
2023-03-23 10:02:09 +0100

    Updated migration SQL file

--- src/main/resources/db/changelog/second-changelog.sql
+++ src/main/resources/db/changelog/second-changelog.sql
@@ -2,7 +2,7 @@
 
 --changeset JediSebas:3
 --comment: Create address table
-CREATE TABLE IF NOT EXIST address(
+CREATE TABLE IF NOT EXISTS address(
 id INT PRIMARY KEY AUTO_INCREMENT,
 street VARCHAR(85),
 house_number VARCHAR(20),
@@ -11,12 +11,22 @@ city VARCHAR(85)
 
 --changeset JediSebas:4
 --comment: Migrate data
-INSERT INTO address (house_number, city, street)
-VALUES (
-(SELECT split_part(address, ' ', 1) FROM personal_details),
-(SELECT split_part(address, ' ', 2) FROM personal_details),
-(SELECT split_part(address, ' ', 3) FROM personal_details)
-);
+--INSERT INTO address (house_number, city, street)
+--VALUES (
+--(SELECT SUBSTRING(address FROM 1 FOR POSITION(' ' IN address) - 1) FROM personal_details),
+--(SELECT SUBSTRING(
+--        address,
+--        POSITION(' ' IN address) + 1,
+--        POSITION(' ' IN address, POSITION(' ' IN address) + 1) - POSITION(' ' IN address) - 1
+--        )  FROM personal_details),
+--(SELECT SUBSTRING(
+--        address,
+--        POSITION(' ' IN address, POSITION(' ' IN address) + 1) - POSITION(' ' IN address) - 1,
+--        LENGTH(address)
+--        ) FROM personal_details)
+--);
+INSERT INTO address (city)
+SELECT address FROM personal_details;
 
 --changeset JediSebas:5
 --comment: Delete address column
@@ -26,5 +36,15 @@ DROP COLUMN address;
 --changeset JediSebas:6
 --comment: Added address_id column
 ALTER TABLE personal_details
-ADD CONSTRAINT address_id
-FOREIGN KEY (address_id) REFERENCES address(id);
+ADD address_id INT;
+
+--changeset JediSebas:7
+--comment: Added address_id as fk
+ALTER TABLE personal_details
+ADD FOREIGN KEY (address_id)
+REFERENCES address (id);
+
+--changeset JediSebas:8
+--comment: Insert address_id
+INSERT INTO personal_details (address_id)
+SELECT id FROM address;
