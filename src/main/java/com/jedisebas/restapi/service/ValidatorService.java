c8852b00475ca4dcc650b22ac163783aab72ffa9
sebastian.dolata <sebasdolata04@gmail.com>
2023-03-23 10:28:45 +0100

    Deleted address from validator

--- src/main/java/com/jedisebas/restapi/service/ValidatorService.java
+++ src/main/java/com/jedisebas/restapi/service/ValidatorService.java
@@ -9,18 +9,15 @@ import java.util.regex.Pattern;
 public class ValidatorService {
 
     public void validatePersonalDetailsDtoFields(final PersonalDetailsDto personalDto) {
-        if (personalDto.getFirstName() == null || personalDto.getLastName() == null ||
-                personalDto.getAddress() == null || personalDto.getEmail() == null) {
+        if (personalDto.getFirstName() == null || personalDto.getLastName() == null || personalDto.getEmail() == null) {
             throw new IllegalArgumentException();
         }
 
-        if (personalDto.getFirstName().isEmpty() || personalDto.getLastName().isEmpty() ||
-                personalDto.getAddress().isEmpty() || personalDto.getEmail().isEmpty()) {
+        if (personalDto.getFirstName().isEmpty() || personalDto.getLastName().isEmpty() || personalDto.getEmail().isEmpty()) {
             throw new IllegalArgumentException();
         }
 
-        if (personalDto.getFirstName().length() > 50 || personalDto.getLastName().length() > 50 ||
-                personalDto.getEmail().length() > 50) {
+        if (personalDto.getFirstName().length() > 50 || personalDto.getLastName().length() > 50 || personalDto.getEmail().length() > 50) {
             throw new IllegalArgumentException();
         }
 
