5fee1305e0dacfea201db9bb4c58990f2ed153c3
sebastian.dolata <sebasdolata04@gmail.com>
2023-03-24 14:27:10 +0100

    Added create event

--- src/main/java/com/jedisebas/restapi/controller/EventController.java
+++ src/main/java/com/jedisebas/restapi/controller/EventController.java
@@ -1,6 +1,12 @@
 package com.jedisebas.restapi.controller;
 
+import com.jedisebas.restapi.dto.CreatedEventResponse;
+import com.jedisebas.restapi.dto.EventDto;
+import com.jedisebas.restapi.service.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
         produces = MediaType.APPLICATION_JSON_VALUE)
 public class EventController {
 
+    @Autowired
+    private EventService service;
 
+    @PostMapping
+    public CreatedEventResponse createEvent(@RequestBody EventDto eventDto) {
+        return service.createEvent(eventDto);
+    }
 }
