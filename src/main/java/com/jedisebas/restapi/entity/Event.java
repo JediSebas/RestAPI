f0340fcb58142e742a1f23d0ab0c5e08a31a6f34
sebastian.dolata <sebasdolata04@gmail.com>
2023-03-24 10:49:06 +0100

    Added Event entity

---
+++ src/main/java/com/jedisebas/restapi/entity/Event.java
@@ -0,0 +1,34 @@
+package com.jedisebas.restapi.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Table(name = "event")
+@Getter
+@Setter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    private String title;
+    private String date;
+    private String description;
+}
