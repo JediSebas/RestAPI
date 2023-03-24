190d8002e55c75e1e6fbc73cd909f8050a8f11dd
sebastian.dolata <sebasdolata04@gmail.com>
2023-03-24 13:46:17 +0100

    Added event mapper

---
+++ src/main/java/com/jedisebas/restapi/mapper/EventMapper.java
@@ -0,0 +1,25 @@
+package com.jedisebas.restapi.mapper;
+
+import com.jedisebas.restapi.dto.EventDto;
+import com.jedisebas.restapi.entity.Event;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EventMapper {
+
+    public Event dtoToEntity(EventDto eventDto) {
+        return Event.builder()
+                .title(eventDto.getTitle())
+                .date(eventDto.getDate())
+                .description(eventDto.getDescription())
+                .build();
+    }
+
+    public EventDto entityToDto(Event event) {
+        return EventDto.builder()
+                .title(event.getTitle())
+                .date(event.getDate())
+                .description(event.getDescription())
+                .build();
+    }
+}
