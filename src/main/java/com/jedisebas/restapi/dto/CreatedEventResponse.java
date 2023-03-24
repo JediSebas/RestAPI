4beae56cb3013885498b15f30d1e9a7194b1834a
sebastian.dolata <sebasdolata04@gmail.com>
2023-03-24 13:14:57 +0100

    Added event response

---
+++ src/main/java/com/jedisebas/restapi/dto/CreatedEventResponse.java
@@ -0,0 +1,11 @@
+package com.jedisebas.restapi.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CreatedEventResponse {
+
+    private int id;
+}
