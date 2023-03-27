d2c513db3fb8b3651e4acc2bd5fba36369112a09
sebastian.dolata <sebasdolata04@gmail.com>
2023-03-27 14:49:56 +0200

    Added new tests for validator

--- src/test/java/com/jedisebas/restapi/service/ValidatorServiceTest.java
+++ src/test/java/com/jedisebas/restapi/service/ValidatorServiceTest.java
@@ -25,10 +25,8 @@ class ValidatorServiceTest {
         assertThrows(IllegalArgumentException.class, () -> validator.validatePersonalDetailsDtoFields(nullFieldsDto));
         assertThrows(IllegalArgumentException.class, () -> validator.validatePersonalDetailsDtoFields(emptyFieldsDto));
         assertThrows(IllegalArgumentException.class, () -> validator.validatePersonalDetailsDtoFields(oneSpaceFieldsDto));
-
-//      I'm not sure how it should works, I'll think later
-//        assertThrows(IllegalArgumentException.class, () -> validator.validatePersonalDetailsDtoFields(nullAddressFieldsDto));
-//        assertThrows(IllegalArgumentException.class, () -> validator.validatePersonalDetailsDtoFields(nullDto));
+        assertThrows(IllegalArgumentException.class, () -> validator.validatePersonalDetailsDtoFields(nullAddressFieldsDto));
+        assertThrows(IllegalArgumentException.class, () -> validator.validatePersonalDetailsDtoFields(nullDto));
     }
 
 }
