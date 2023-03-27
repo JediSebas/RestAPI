c3b1cb1fd236af959a954cc201b2776940ac1f47
sebastian.dolata <sebasdolata04@gmail.com>
2023-03-27 19:31:06 +0200

    Feature update

--- src/main/java/com/jedisebas/restapi/controller/PersonalDetailsController.java
+++ src/main/java/com/jedisebas/restapi/controller/PersonalDetailsController.java
@@ -6,13 +6,7 @@ import com.jedisebas.restapi.service.PersonalDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -39,5 +33,10 @@ public class PersonalDetailsController {
     @GetMapping("/{id}")
     public PersonalDetailsDto getById(@PathVariable int id) {
         return service.fetchPersonalDetails(id);
+    }
+
+    @PutMapping("/{id}")
+    public PersonalDetailsDto updateById(@PathVariable final int id, @RequestBody final PersonalDetailsDto personalDto) {
+        return service.updatePersonalDetails(id, personalDto);
     }
 }
--- src/main/java/com/jedisebas/restapi/service/PersonalDetailsService.java
+++ src/main/java/com/jedisebas/restapi/service/PersonalDetailsService.java
@@ -3,6 +3,7 @@ package com.jedisebas.restapi.service;
 import com.jedisebas.restapi.dto.CreatedEntityResponse;
 import com.jedisebas.restapi.dto.PersonalDetailsDto;
 import com.jedisebas.restapi.entity.PersonalDetails;
+import com.jedisebas.restapi.mapper.AddressMapper;
 import com.jedisebas.restapi.mapper.PersonalDetailsMapper;
 import com.jedisebas.restapi.repository.PersonalDetailsRepository;
 import lombok.RequiredArgsConstructor;
@@ -19,6 +20,7 @@ public class PersonalDetailsService {
 
     private final PersonalDetailsRepository repository;
     private final PersonalDetailsMapper mapper;
+    private final AddressMapper addressMapper;
     private final ValidatorService validator;
 
     public CreatedEntityResponse createPersonalDetails(final PersonalDetailsDto personalDto) {
@@ -43,5 +45,23 @@ public class PersonalDetailsService {
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "person not found"));
 
         return mapper.entityToDto(persons);
+    }
+
+    public PersonalDetailsDto updatePersonalDetails(final int id, final PersonalDetailsDto personalDto) {
+        validator.validatePersonalDetailsDtoFields(personalDto);
+
+        final PersonalDetails foundedPerson = repository
+                .findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "person not found"));
+
+        foundedPerson.setFirstName(personalDto.getFirstName());
+        foundedPerson.setLastName(personalDto.getLastName());
+        foundedPerson.setEmail(personalDto.getEmail());
+        foundedPerson.setAddress(addressMapper.dtoToEntity(personalDto.getAddress()));
+
+        repository.updateById(foundedPerson.getFirstName(), foundedPerson.getLastName(), foundedPerson.getAddress(),
+                foundedPerson.getEmail(), foundedPerson.getId());
+
+        return mapper.entityToDto(foundedPerson);
     }
 }
