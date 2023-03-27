fd075fbd147cc086b20cf72f5eaf8668d71eaeab
sebastian.dolata <sebasdolata04@gmail.com>
2023-03-27 14:34:37 +0200

    Added new tests for validator

--- src/test/java/com/jedisebas/restapi/service/ValidatorServiceTest.java
+++ src/test/java/com/jedisebas/restapi/service/ValidatorServiceTest.java
@@ -1,15 +1,32 @@
 package com.jedisebas.restapi.service;
 
+import org.junit.jupiter.api.Test;
+
+import com.jedisebas.restapi.constants.TestDataProvider;
 import com.jedisebas.restapi.dto.PersonalDetailsDto;
+
+import static org.junit.Assert.assertThrows;
 
 class ValidatorServiceTest {
 
+    @Test
     void givenPersonalDetailsDto_whenValidate() {
-        PersonalDetailsDto validDto;
+        ValidatorService validator = new ValidatorService();
+
+        PersonalDetailsDto validDto = TestDataProvider.createValidPersonalDetailsDto();
         PersonalDetailsDto tooLongFieldDto;
-        PersonalDetailsDto nullFieldDto;
-        PersonalDetailsDto emptyFieldDto;
+        PersonalDetailsDto notValidEmailDto;
+        PersonalDetailsDto nullFieldsDto = TestDataProvider.createNullFieldsPersonalDetailsDto();
+        PersonalDetailsDto emptyFieldsDto = TestDataProvider.createEmptyFieldsPersonalDetailsDto();
+        PersonalDetailsDto oneSpaceFieldsDto = TestDataProvider.createOneSpaceFieldsPersonalDetailsDto();
+        PersonalDetailsDto nullAddressFieldsDto = TestDataProvider.createNullAddressFieldsPersonalDetailsDto();
         PersonalDetailsDto nullDto = null;
+
+        assertThrows(IllegalArgumentException.class, () -> validator.validatePersonalDetailsDtoFields(nullFieldsDto));
+        assertThrows(IllegalArgumentException.class, () -> validator.validatePersonalDetailsDtoFields(emptyFieldsDto));
+        assertThrows(IllegalArgumentException.class, () -> validator.validatePersonalDetailsDtoFields(oneSpaceFieldsDto));
+        assertThrows(IllegalArgumentException.class, () -> validator.validatePersonalDetailsDtoFields(nullAddressFieldsDto));
+        assertThrows(IllegalArgumentException.class, () -> validator.validatePersonalDetailsDtoFields(nullDto));
     }
 
 }
