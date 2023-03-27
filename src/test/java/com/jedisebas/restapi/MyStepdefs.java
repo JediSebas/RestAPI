45457337f4a0c0a9fad0a0b6aac14e33d4529a4d
sebastian.dolata <sebasdolata04@gmail.com>
2023-03-27 13:56:57 +0200

    The last time when test pass

---
+++ src/test/java/com/jedisebas/restapi/MyStepdefs.java
@@ -0,0 +1,16 @@
+package com.jedisebas.restapi;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class MyStepdefs {
+
+    @Given("a GET request to {string}")
+    public void aGETRequestTo(String arg0) {
+
+    }
+
+    @Then("the response status code should be {int} OK")
+    public void theResponseStatusCodeShouldBeOK(int arg0) {
+    }
+}
--- src/test/resources/Event.feature
+++ src/test/resources/Event.feature
@@ -2,3 +2,7 @@ Feature: Testing Event POST
   Scenario: POST request to /v1/events returns 201 CREATED
     Given a POST request to "/v1/events"
     Then the response status code should be 201 CREATED
+
+  Scenario: GET request to /v1/events returns 200 OK
+    Given a GET request to "/v1/events"
+    Then the response status code should be 200 OK
