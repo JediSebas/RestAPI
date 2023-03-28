143fd1d1e4482eaac6fa78bfed1e7f6c56d24442
sebastian.dolata <sebasdolata04@gmail.com>
2023-03-28 14:29:24 +0200

    Fix

--- src/functional/java/com/jedisebas/restapi/EndToEndEventTest.java
+++ src/functional/java/com/jedisebas/restapi/EndToEndEventTest.java
@@ -11,7 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class EndToEndEventTest {
 
     private RequestSpecification request;
