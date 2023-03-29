acf7ee0029ccdbaca93e72b1928925538373e310
sebastian.dolata <sebasdolata04@gmail.com>
2023-03-29 09:24:53 +0200

    Added http 400 code test

--- src/test/java/com/jedisebas/restapi/RestApiApplicationTests.java
+++ src/test/java/com/jedisebas/restapi/RestApiApplicationTests.java
@@ -10,8 +10,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -43,5 +45,14 @@ class RestApiApplicationTests {
 		final PersonalDetailsDto testIfWorks = service.fetchPersonalDetails(1);
 
 		assertEquals(personalDetailsDto.getEmail(), testIfWorks.getEmail());
+	}
+
+	@Test
+	void personalDetailsCreateThrowException() throws Exception {
+		final MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/v1/persons")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(JsonRequestProvider.EMPTY_PERSONAL_DETAILS_JSON)).andReturn();
+
+		assertEquals(HttpStatus.BAD_REQUEST.value(), mvcResult.getResponse().getStatus());
 	}
 }
